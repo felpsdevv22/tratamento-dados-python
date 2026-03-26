60d2beee021ae202f7e4c247dd27a198040458b7
Felipe Almeida Pereira <felps3rror@gmail.com>
2026-03-26 10:13:37 -0300

    feat: tratamento de dados com pandas e exportação para Excel att

--- automacao.py
+++ automacao.py
@@ -1,13 +1,28 @@
 import pandas as pd
-df = pd.read_csv("dados.csv")
+
+def carregar_dados(): 
+    try: 
+        return pd.read_csv("dados.csv")
+    except Exception as e:
+       print("Erro ao carregar arquivo: ", e)
+       return None
+
+df = carregar_dados()
+
+if df is not None:
+    print("Dados carregados com sucesso")    
 
 antes = len(df)
+
 df =df.drop_duplicates()
-depois = len(df)
 
 df = df[df['email'].str.contains('@')]
+
+depois = len(df)
+
 df.to_excel("dados_tratados.xlsx", index=False)
 
 print("✅ Processo concluido")
 print(f"Registros originais: {antes}")
 print(f"Registros finais: {depois}")
+
